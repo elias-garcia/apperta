afc59b243ed986480887c1c5fab4c6fc4c127154
Elías García Mariño <elias.garcia@netexlearning.com>
2019-06-05 16:29:44 +0200

    fixed payment-rates and env vars

--- server/src/api/payment/payment.controller.js
+++ server/src/api/payment/payment.controller.js
@@ -2,13 +2,13 @@ const ApiError = require('../api-error');
 const paymentService = require('./payment.service');
 const json = require('../../util/json');
 const dto = require('../../util/dto');
-const appConfig = require('../../config/app.config');
+const paymentRates = require('./payment-rates');
 
 const create = async (req, res, next) => {
   try {
     if (!req.body.token
       || req.body.paymentRate < 0
-      || (req.body.paymentRate > (appConfig.rates.length - 1))) {
+      || (req.body.paymentRate > (paymentRates.length - 1))) {
       throw new ApiError(422, 'unprocessable entity');
     }
 
